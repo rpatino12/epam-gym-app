c1310d9c04c3260b875e1d75b529d9e9eee17f77
rpatino12 <rpatino12@outlook.com>
2023-12-14 11:49:17 -0500

    Initial commit, add project files generated with spring initializr

---
+++ src/test/java/com/rpatino12/epam/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpatino12.epam.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
