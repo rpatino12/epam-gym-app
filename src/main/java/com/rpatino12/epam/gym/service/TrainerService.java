826de19d190a8e43361925eb5dd4a1dd8c71e7b6
rpatino12 <rpatino12@outlook.com>
2023-12-17 17:44:29 -0500

    Include logging class attribute to service classes

--- src/main/java/com/rpatino12/epam/gym/service/TrainerService.java
+++ src/main/java/com/rpatino12/epam/gym/service/TrainerService.java
@@ -1,6 +1,8 @@
 package com.rpatino12.epam.gym.service;
 
 import com.rpatino12.epam.gym.dao.TrainerDAO;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Service;
 public class TrainerService {
     @Autowired
     private TrainerDAO trainerDAO;
+    private static final Log LOG = LogFactory.getLog(TrainerService.class);
+
 }
--- src/main/java/com/rpatino12/epam/gym/service/TrainingService.java
+++ src/main/java/com/rpatino12/epam/gym/service/TrainingService.java
@@ -1,6 +1,8 @@
 package com.rpatino12.epam.gym.service;
 
 import com.rpatino12.epam.gym.dao.TrainingDAO;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,5 @@ import org.springframework.stereotype.Service;
 public class TrainingService {
     @Autowired
     private TrainingDAO trainingDAO;
+    private static final Log LOG = LogFactory.getLog(TrainingService.class);
 }
