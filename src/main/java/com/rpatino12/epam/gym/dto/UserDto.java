c8a0c54e8c90c746695c6723a3b7e59ff0758a10
rpatino12 <rpatino12@outlook.com>
2024-02-05 16:49:31 -0500

    Add user, trainee and trainer DTO classes

---
+++ src/main/java/com/rpatino12/epam/gym/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.rpatino12.epam.gym.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String username;
+    private String firstName;
+    private String lastName;
+}
